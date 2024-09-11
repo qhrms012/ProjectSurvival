a7de0a7c14f8a246de76c2a62c6dd2d42c9fb513
boguen <qhrms012@naver.com>
2024-09-11 21:25:31 +0900

    [add] 에셋 추가 및 가까운거리 적 측정

--- Assets/01.Scripts/Enemy/Scanner.cs
+++ Assets/01.Scripts/Enemy/Scanner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Scanner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float scanRange;
+    public LayerMask targetLayer;
+    public RaycastHit2D[] targets;
+    public Transform nearestTarget;
+
+    private void FixedUpdate()
     {
-        
+        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer);
+        nearestTarget = GetNearest();
     }
 
-    // Update is called once per frame
-    void Update()
+    Transform GetNearest()
     {
-        
+        Transform result = null;
+        float diff = 100;
+
+        foreach(RaycastHit2D target in targets)
+        {
+            Vector3 myPos = transform.position;
+            Vector3 targetPos = target.transform.position;
+            float curDiff = Vector3.Distance(myPos, targetPos);
+
+            if(curDiff < diff)
+            {
+                diff = curDiff;
+                result = target.transform;
+            }
+        }
+
+
+        return result;
     }
 }
