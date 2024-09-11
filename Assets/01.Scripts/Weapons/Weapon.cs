8e6ccfd427af46cdd7be96c5450c1c66b98bdbe4
boguen <qhrms012@naver.com>
2024-09-11 18:05:55 +0900

    [add] 무기 스크립트 테스트 스캐너 스크립트

--- Assets/01.Scripts/Weapons/Weapon.cs
+++ Assets/01.Scripts/Weapons/Weapon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Weapon : MonoBehaviour
 {
@@ -28,6 +29,8 @@ public class Weapon : MonoBehaviour
                 break;
 
         }
+        
+            
     }
 
     public void Init()
@@ -44,19 +47,46 @@ public class Weapon : MonoBehaviour
                 
         }
     }
+    public void LevelUp(float damage, int count)
+    {
+        this.damage = damage;
+        this.count += count;
+
+        if (id == 0)
+            WeaponCount();
+            
+    }
+
 
     private void WeaponCount()
     {
         for (int index = 0; index < count; index++)
         {
-            Transform bullet = GameManager.Instance.ObjectPool.Get(prefabId).transform;
-            bullet.parent = transform;
+            Transform bullet;
+            if(index < transform.childCount)
+            {
+                bullet = transform.GetChild(index);
+            }
+            else
+            {
+                bullet = GameManager.Instance.ObjectPool.Get(prefabId).transform;
+                bullet.parent = transform;
+            }
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
 
             Vector3 rotVec = Vector3.forward * 360 * index / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 1.5f, Space.World);
 
             bullet.GetComponent<Bullet>().Init(damage, -1); // 무한 관통 무기
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            LevelUp(20, 5);
         }
     }
 }
