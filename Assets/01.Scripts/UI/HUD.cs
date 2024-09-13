43349cc9b5101934bb90ece542907c3075daca18
boguen <qhrms012@naver.com>
2024-09-13 20:40:32 +0900

    [add] UI 슬라이더 경험치바 생성

---
+++ Assets/01.Scripts/UI/HUD.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType { Exp, Level, Kill, Time, Health}
+    public InfoType type;
+
+
+    TextMeshProUGUI myText;
+    Slider mySlider;
+
+    private void Awake()
+    {
+        mySlider = GetComponent<Slider>();
+        myText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+
+                break;
+            case InfoType.Level:
+
+                break;
+            case InfoType.Kill:
+
+                break;
+            case InfoType.Time:
+
+                break;
+            case InfoType.Health:
+
+                break;
+
+        }
+    }
+}
