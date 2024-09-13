ca5f92b03de41d7ade3e2fe50d77c079facad9a9
boguen <qhrms012@naver.com>
2024-09-13 21:05:05 +0900

    [add] 타이머 설정

--- Assets/01.Scripts/Manager/GameManager.cs
+++ Assets/01.Scripts/Manager/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : Singleton<GameManager>
     public float maxGameTime = 2 * 10f;
 
     [Header("Player Info")]
+    public int health;
+    public int maxHealth;
     public int level;
     public int kill;
     public int exp;
--- Assets/01.Scripts/UI/HUD.cs
+++ Assets/01.Scripts/UI/HUD.cs
@@ -30,13 +30,16 @@ public class HUD : MonoBehaviour
 
                 break;
             case InfoType.Level:
-
+                myText.text = string.Format("Lv.{0:F0}", GameManager.Instance.level);
                 break;
             case InfoType.Kill:
-
+                myText.text = string.Format("{0:F0}", GameManager.Instance.kill);
                 break;
             case InfoType.Time:
-
+                float remainTime = GameManager.Instance.maxGameTime - GameManager.Instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
                 break;
             case InfoType.Health:
 
