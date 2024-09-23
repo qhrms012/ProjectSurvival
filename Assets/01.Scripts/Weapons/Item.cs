e772e6a5cfb0f30dcf1b56e1d5ada9dbb1016a87
boguen <qhrms012@naver.com>
2024-09-23 21:50:08 +0900

    [add] 버튼 메서드 생성

--- Assets/01.Scripts/Weapons/Item.cs
+++ Assets/01.Scripts/Weapons/Item.cs
@@ -26,4 +26,29 @@ public class Item : MonoBehaviour
     {
         textLevel.text = "Lv." + (level + 1);
     }
+
+    public void OnClick()
+    {
+        switch(data.itemType)
+        {
+            case ItemData.ItemType.Melee:
+            case ItemData.ItemType.Range:
+
+                break;
+            
+            case ItemData.ItemType.Glove:
+                break;
+            case ItemData.ItemType.Shoe:
+                break;
+            case ItemData.ItemType.Heal:
+                break;
+        }
+
+        level++;
+
+        if (level == data.damages.Length)
+        {
+            GetComponent<Button>().interactable = false;
+        }
+    }
 }
