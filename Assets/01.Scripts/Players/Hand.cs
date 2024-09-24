d4a1e62bd747e5048b05da92689c71e8a676640a
boguen <qhrms012@naver.com>
2024-09-25 00:07:23 +0900

    [add] 왼손 오른손 위치 조정

---
+++ Assets/01.Scripts/Players/Hand.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public bool isLeft;
+    public SpriteRenderer spriter;
+
+    SpriteRenderer player;
+
+    Vector3 rightPos = new Vector3(0.09f, -0.11f, 0);
+    Vector3 rightPosReverse = new Vector3(-0.09f, -0.11f, 0);
+    Quaternion leftRot = Quaternion.Euler(0.06f, -0.14f, -100);
+    Quaternion leftRotReverse = Quaternion.Euler(0, 0, -100);
+
+    private void Awake()
+    {
+        player = GetComponentsInParent<SpriteRenderer>()[1];
+    }
+
+    private void LateUpdate()
+    {
+        bool isReverse = player.flipX;
+
+        if (isLeft)
+        {
+            transform.localRotation = isReverse ? leftRotReverse : leftRot;
+            spriter.flipY = isReverse;
+            spriter.sortingOrder = isReverse ? 4 : 6;
+        }
+        else
+        {
+            transform.localPosition = isReverse ? rightPosReverse : rightPos;
+            spriter.flipX = isReverse;
+            spriter.sortingOrder = isReverse ? 6 : 4;
+        }
+    }
+}
