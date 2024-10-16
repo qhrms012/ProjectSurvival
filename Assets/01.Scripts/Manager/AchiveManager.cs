459f39f283211e417cbaaa1dc01a915ea0c67cbe
boguen <qhrms012@naver.com>
2024-10-16 23:19:45 +0900

    [add] 인스펙터 연결

--- Assets/01.Scripts/Manager/AchiveManager.cs
+++ Assets/01.Scripts/Manager/AchiveManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AchiveManager : MonoBehaviour
 {
+    public GameObject[] lockCharacter;
+    public GameObject[] unlockCharacter;
     // Start is called before the first frame update
     void Start()
     {
