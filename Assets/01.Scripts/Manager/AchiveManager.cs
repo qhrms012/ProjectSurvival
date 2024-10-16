8c5ab59dd4805e71021b59309045ce0dc04fc7a1
boguen <qhrms012@naver.com>
2024-10-16 23:29:55 +0900

    [add] 캐릭터 해금 조건

--- Assets/01.Scripts/Manager/AchiveManager.cs
+++ Assets/01.Scripts/Manager/AchiveManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,43 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+
+    enum Achive { UnlockFrog, UnlockMask}
+    Achive[] achives;
+
+    private void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+
+        if(!PlayerPrefs.HasKey("MyData"))
+            Init();
+    }
+
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData" , 1);
+
+        foreach (Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnlockCharacter();
     }
 
+    void UnlockCharacter()
+    {
+        for (int index = 0; index < lockCharacter.Length; index++)
+        {
+            string achiveName = achives[index].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1;
+            lockCharacter[index].SetActive(!isUnlock);
+            unlockCharacter[index].SetActive(isUnlock);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
