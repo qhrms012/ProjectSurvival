c70d93ae84d691c79a0ea4838a9d324de28735cb
boguen <qhrms012@naver.com>
2024-10-22 22:54:55 +0900

    [fix] 맵 재배치

--- Assets/01.Scripts/Map/Reposition.cs
+++ Assets/01.Scripts/Map/Reposition.cs
@@ -22,16 +22,21 @@ public class Reposition : MonoBehaviour
 
         Vector3 playerPos = GameManager.Instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float diffX = Mathf.Abs(playerPos.x - myPos.x);
-        float diffY = Mathf.Abs(playerPos.y - myPos.y);
-
-        Vector3 playerDir = GameManager.Instance.player.playerVector;
-        float dirX = playerDir.x < 0 ? -1 : 1;
-        float dirY = playerDir.y < 0 ? -1 : 1;
+        
 
         switch (transform.tag)
         {
             case "Ground":
+                float diffX = playerPos.x - myPos.x;
+                float diffY = playerPos.y - myPos.y;
+
+                //Vector3 playerDir = GameManager.Instance.player.playerVector;
+                float dirX = diffX < 0 ? -1 : 1;
+                float dirY = diffY < 0 ? -1 : 1;
+
+                diffX = Mathf.Abs(diffX);
+                diffY = Mathf.Abs(diffY);
+
                 if (diffX > diffY)
                 {
                     transform.Translate(Vector3.right * dirX * 40);
